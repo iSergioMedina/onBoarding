950e232c86fa83a061d3cec5c7f1232c127a6360
iSergioMedina <108115960+iSergioMedina@users.noreply.github.com>
2022-12-05 03:24:09 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,53 +1,51 @@
-yargs.command(\{\
-    command: 'add',\
-    describe: \'Agrega una nota'\
-    builder: \{\
-        title: \{\
-            describe: \'Titulo de la nota',\
-            demandOption: true,\
-            type: 'string'\
-        \},\
-        body: \{\
-            describe: \'Cuerpo de la nota\'92,\
-            demandOption: true,\
-            type: 'string'\
-        \}\
-    \},\
-    handler(argv) \{\
-        notes.addNote(argv.title, argv.body)\
-    \}\
-\})\
-\
-yargs.command(\{\
-    command: 'remove',\
-    describe: \'Eliminar Nota,\
-    builder: \{\
-        title: \{\
-            describe: \'Titulo de la nota\'92,\
-            demandOption: true,\
-            type: 'string'\
-        \}\
-    \},\
-    handler(argv) \{\
-        notes.removeNote(argv.title)\
-    \}\
-\})\
-\
-\
-// Create read command\
-yargs.command(\{\
-    command: 'read',\
-    describe: \'Leer una nota\'92,\
-    builder: \{\
-        title: \{\
-            describe: \'Titulo de la nota\'92,\
-            demandOption: true,\
-            type: 'string'\
-        \}\
-    \},\
-    handler(argv) \{\
-        notes.readNote(argv.title)\
-    \}\
-\})\
-\
+yargs.command({
+    command: 'add',
+    describe: 'Agrega una nota'
+    builder: {
+        title: {
+            describe: 'Titulo de la nota',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Cuerpo de la nota',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Eliminar Nota,
+    builder: {
+        title: {
+            describe: 'Titulo de la nota',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Leer una nota',
+    builder: {
+        title: {
+            describe: 'Titulo de la nota',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
 yargs.parse()}
